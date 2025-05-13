77eee4a6145e9d71d61a9eaa7bffca102ebc475b
NRafael Cunha <rafael.cunha@senior.com.br>
2025-05-13 17:17:29 -0300

    criação get e set Category

--- src/main/java/com/rafaelcunha/desafio_anota_ai/domain/category/Category.java
+++ src/main/java/com/rafaelcunha/desafio_anota_ai/domain/category/Category.java
@@ -22,4 +22,36 @@ public class Category {
         this.description = categoryData.description();
         this.ownerId = categoryData.ownerId();
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
 }
